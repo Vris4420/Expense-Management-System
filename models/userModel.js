9c49aac22175bdeff8275a8e517302f535363552
Vrishank Sharma <sharma08462@gmail.com>
2024-03-29 16:04:25 +0530

    userRoutes created

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
-const {Schema, model} = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-    name:{
-        type:String,
-        required:[true, 'Name required']
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name required"],
     },
-    email:{
-        type:String,
-        required:[true,'Unique email required'],
-        unique:ture
+    email: {
+      type: String,
+      required: [true, "Unique email required"],
+      unique: ture,
     },
-    password:{
-        type:String,
-        required:[true,'Password required'],
-    }
-},{timestamps:true})
+    password: {
+      type: String,
+      required: [true, "Password required"],
+    },
+  },
+  { timestamps: true }
+);
 
-const userModel = model('users', userSchema);
+const userModel = model("users", userSchema);
 module.exports = userModel;
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const connectDb = require('./config/connectDB');
+const cors = require("cors");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const connectDb = require("./config/connectDB");
 dotenv.config();
 
 connectDb();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req,res) => {
-    res.send("hello");
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
 const PORT = 8080 || process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
-})
+  console.log(`Server is listening on port ${PORT}`);
+});
