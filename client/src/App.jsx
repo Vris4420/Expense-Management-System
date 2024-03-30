b328c731f95fb95579e0d05ca4b360c51f307f78
Vrishank Sharma <sharma08462@gmail.com>
2024-03-31 00:00:00 +0530

    antd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Register from './pages/Register';
 
 function App() {
  
@@ -8,6 +9,7 @@ function App() {
     <>
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route path='/register' element={<Register />} />
     </Routes>
     </>
   )
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Form, Input} from 'antd'
+
+const Register = () => {
+    return (
+        <div className="register-page">
+            <Form layout="vertical">
+                <h1>Registration Page</h1>
+                <Form.Item label="Name" name="name">
+                    <Input/>
+                </Form.Item>
+            </Form>
+        </div>
+    )
+}
+
+export default Register;
